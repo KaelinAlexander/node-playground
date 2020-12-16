fe59ed53e8ebf7e02a18c62080137a04444ca9d8
KaelinAlexander <kaelin.alexander@gmail.com>
2020-12-16 09:14:22 -0700

    initial explanation of exports, imports, and os object

---
+++ modules.js
@@ -0,0 +1,9 @@
+const { people, ages } = require('./people')
+// const xyz = require('./people')
+
+console.log(people, ages)
+// console.log(xyz.ages)
+
+const os = require('os');
+
+console.log(os.platform(), os.homedir())
