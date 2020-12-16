d11857df62b8e83e45f743465d08e039e9a88ed3
KaelinAlexander <kaelin.alexander@gmail.com>
2020-12-15 18:24:04 -0700

    adds more examples of global object methods

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-console.log('test message')
+// console.log('test message')
 
-setTimeout(function(){
-    console.log('three seconds have passed')
-}, 3000)
+// setTimeout(function(){
+//     console.log('three seconds have passed')
+// }, 3000)
+
+// let time = 0;
+
+// const timer = setInterval(function(){
+//     time += 2;
+//     console.log(time + ' seconds have passed');
+//     if (time > 5){
+//         clearInterval(timer)
+//     }
+// }, 2000)
+
+console.log(__dirname)
+console.log(__filename)
