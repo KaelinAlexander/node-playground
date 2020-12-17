b3385217205580460a21b495e6d887d273dc2776
KaelinAlexander <kaelin.alexander@gmail.com>
2020-12-17 10:05:53 -0700

    adds basic response via the fs module

--- video4/server.js
+++ video4/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
@@ -6,10 +7,18 @@ const server = http.createServer((req, res) => {
     // set header content type
     res.setHeader('Content-Type', 'text/html');
 
-    res.write('<head><link rel="stylesheet" href="#"></head>')
-    res.write('<p>Hello there! This is HTML.</p>');
-    res.write('<p>Hello again! This is a second HTML p tag.</p>');
-    res.end();
+    // send an html file
+
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            // res.write(data);
+            res.end(data);
+        }
+    })
+
 });
 
 server.listen(3000, 'localhost', () => {
