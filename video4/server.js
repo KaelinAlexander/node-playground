dd3eba07c3f7963d54beef99459df644ff019e92
KaelinAlexander <kaelin.alexander@gmail.com>
2020-12-17 10:21:54 -0700

    adds basic router

--- video4/server.js
+++ video4/server.js
@@ -7,9 +7,22 @@ const server = http.createServer((req, res) => {
     // set header content type
     res.setHeader('Content-Type', 'text/html');
 
-    // send an html file
+    let path = './views/';
 
-    fs.readFile('./views/index.html', (err, data) => {
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+
+    // send an html file
+    fs.readFile(path, (err, data) => {
         if (err) {
             console.log(err);
             res.end();
