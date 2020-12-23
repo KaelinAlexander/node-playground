2d712b5ff9e6bcf72c95175f9c2bc470345b0096
KaelinAlexander <kaelin.alexander@gmail.com>
2020-12-23 10:14:31 -0700

    wires connection to database; refactors port listening

--- video9/app.js
+++ video9/app.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 // express app
 
 const app = express();
 
+// connect to mongodb
+
+const dbURI = 'mongodb+srv://kaelintest:testerclef@cluster0.m1lko.mongodb.net/node-tuts?retryWrites=true&w=majority'
+mongoose.connect(dbURI, { useNewUrlParser: true,  useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
 // register view engine; requires app having been made with express
 
 app.set('view engine', 'ejs')
 // app.set('views', 'myviews')
-
-// listen for requests
-
-app.listen(3000)
 
 // middleware & static files
 
