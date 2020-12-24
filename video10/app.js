a1a04f3ed2b61efd715b119710fe9b4961cdb0c8
KaelinAlexander <kaelin.alexander@gmail.com>
2020-12-24 11:07:28 -0700

    adds dynamic routing for a details page; adds details view; modifies index styling

--- video10/app.js
+++ video10/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const Blog = require('./models/blog');
+const { render } = require('ejs');
 
 // express app
 
@@ -87,6 +88,17 @@ app.post('/blogs', (req, res) => {
     blog.save()
     .then((result) => {
         res.redirect('/blogs')
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+    .then(result => {
+        res.render('details', { title: 'Blog Details', blog: result}) 
     })
     .catch((err) => {
         console.log(err);
